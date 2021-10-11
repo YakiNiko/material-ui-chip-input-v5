71bbf2ae84b927b8000be1b618ee15f3c82c1554
Nicolas Grégoire <nicolas.gregoire@hikipeople.com>
2021-10-11 16:06:14 +0200

    chore(mui): fix style import

--- stories/examples/CustomizedChipInput.js
+++ stories/examples/CustomizedChipInput.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import withStyles from '@mui/styles/withStyles'
+import { withStyles }  from '@mui/styles'
 import ChipInput from '../../src/ChipInput'
 
 const styles = {
